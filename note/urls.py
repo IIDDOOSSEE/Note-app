32361cdd220f346321f8d0f58e1dddcf7409c204
Dose <nongdose@gmail.com>
2024-12-12 13:06:15 +0700

    add file

--- note/urls.py
+++ note/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<int:note_id>/", views.detail, name="detail")
+    path("<int:note_id>/", views.detail, name="detail"),
+    path("add/", views.add_note, name="add_note")
 ]
--- note/views.py
+++ note/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import note
-from django.shortcuts import render, get_object_or_404
-
+from django.shortcuts import render, get_object_or_404 ,redirect
+from datetime import datetime
 def index(request):
     latest_note_list = note.objects.order_by("-pub_date")[:5]
     context = {"latest_note_list": latest_note_list}
@@ -12,3 +12,20 @@ def detail(request, note_id):
     latest_note_data = get_object_or_404(note, id=note_id)
     context = {"latest_note_data": latest_note_data}
     return render(request, "note/detail.html", context)
+
+def add_note(request):
+    if request.method == 'POST':
+        
+        title = request.POST.get('title_text')
+        content = request.POST.get('content_text')
+        pub_date = request.POST.get('pub_date')
+
+        if title and content and pub_date:
+            note.objects.create(
+                title_text=title,
+                content_text=content,
+                pub_date=datetime.strptime(pub_date, '%Y-%m-%dT%H:%M')
+            )
+            return redirect('index')  
+
+    return render(request, 'note/add_note.html')
